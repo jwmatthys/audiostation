ff8a3fd2f54475904da74074e2a012f5601ac43b
Joel Matthys <joel@matthysmusic.com>
2019-07-25 15:32:14 -0500

    working example

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rpi-playback-station
+
+Connect button to 3.3V (inside row, 5th pin from USB ports) and board pin 10 (outside row, fifth from opposite end)
+
--- rpi-button-mp3.py
+++ rpi-button-mp3.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 pygame.mixer.init()
 pygame.mixer.music.load("test.mp3")
@@ -12,6 +12,5 @@ pygame.mixer.music.play()
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
-        if pygame.mixer.music.get_busy() == False:
+       if pygame.mixer.music.get_busy() == False:
             pygame.mixer.music.play()
